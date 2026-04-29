30dcfb001be12e580f22ebadab2eaebe70bd5ad1
rameolic <73160905+rameolic@users.noreply.github.com>
2026-04-29 13:54:06 +0530

    macos released

--- macos/Runner/MainFlutterWindow.swift
+++ macos/Runner/MainFlutterWindow.swift
@@ -4,9 +4,10 @@ import FlutterMacOS
 class MainFlutterWindow: NSWindow {
   override func awakeFromNib() {
     let flutterViewController = FlutterViewController()
-    let windowFrame = self.frame
     self.contentViewController = flutterViewController
+    let windowFrame = NSRect(x: 0, y: 0, width: 1920, height: 1080)
     self.setFrame(windowFrame, display: true)
+    self.center()
 
     RegisterGeneratedPlugins(registry: flutterViewController)
 
